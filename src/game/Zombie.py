6e4092ec29c01f9ecf375e7c4a20025b31b59598
John Smith <zmac667@live.rhul.ac.uk>
2024-03-06 16:20:16 +0000

    bcxdsg

--- src/game/Zombie.py
+++ src/game/Zombie.py
@@ -2,6 +2,16 @@ try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-    
+
 class Zombie:
-    def _init__(self, name):
+    def _init__(self, canvas, name):
+        self.canvas = canvas
+        self.name = name
+    
+    def move_towards_player(self):
+        player_coords = self.canvas.coords()
+        zombie_coords = self.canvas.coords(self.image)  
+        if zombie_coords.x < player_coords:
+            zombie_move_right()
+        else:
+            zombie_move_left()
