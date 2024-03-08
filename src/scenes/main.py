d801d8dd219e82bb3c6f8f39a9870e5b1f4d7478
Zolo101 <umadeguy@gmail.com>
2024-03-08 15:51:38 +0000

    Player class

--- src/scenes/main.py
+++ src/scenes/main.py
@@ -1,15 +1,19 @@
 from src.classes.SceneManager import Scene
+from src.game.Player import Player
 from src.game.Sky import Sky
 from src.game.Terrain import Terrain
 
 # TODO: Make the width and height a variable
 terrain = Terrain(1280, 800)
 sky = Sky()
+player = Player(600, 400)
 
 
 def draw(canvas, clock, frame):
     sky.draw(canvas, clock, frame)
     terrain.draw(canvas)
+
+    player.draw(canvas)
 
 
 main = Scene("main", draw)
