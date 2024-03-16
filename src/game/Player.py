00aca7071e05014ce0d69580520d9e2bbc21fda7
rsn558 <ZMAC558@live.rhul.ac.uk>
2024-03-16 19:10:31 +0000

    REPLACED RECTANGLE WITH A SPRITESHEET FOR PLAYER. IGNORE CHOICE OF SPRITESHEET AS IT IS TEMPORARY. CURRENTLY PLAYER IS NOT JUMPING (NEED TO FIX THAT).

--- src/game/Player.py
+++ src/game/Player.py
@@ -1,4 +1,5 @@
 from src.classes.Sprite import Sprite
+from src.classes.Spritesheet import Spritesheet
 
 SPEED = 15
 JUMP_POWER = 24
@@ -8,20 +9,36 @@ class Player:
     def __init__(self, x, y, name, level=1, health=100, max_health=100):
         self.size = (30, 75)
         self.sprite = Sprite("player")
-        self.sprite.pos.x = x
-        self.sprite.pos.y = y
+        # self.spritesheet.pos.x = 300
+        # self.spritesheet.pos.y = 300
+        self.spritesheet = Spritesheet("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a072d29-42bc-45bb-be7b-cd731544eac2/degnka6-ae982415-ae64-485b-a2e9-04d30aeb9141.png/v1/fill/w_640,h_840/wonderland_player_spritesheet_by_littlestarsowo_degnka6-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODQwIiwicGF0aCI6IlwvZlwvOGEwNzJkMjktNDJiYy00NWJiLWJlN2ItY2Q3MzE1NDRlYWMyXC9kZWdua2E2LWFlOTgyNDE1LWFlNjQtNDg1Yi1hMmU5LTA0ZDMwYWViOTE0MS5wbmciLCJ3aWR0aCI6Ijw9NjQwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.b46AhMWfR3ygLqYMAhNkdrAuIsnDj_iKiV52391Nj5w", 640,
+                                       210, 4, 1)
+        self.spritesheet.pos.x = 640
+        self.spritesheet.pos.y = 560
+
+
 
     def update(self, interaction):
         # print(interaction.get_key("space"))
         # convert interaction keys to velocity
         if interaction.get_key("space") and self.sprite.grounded:
             self.sprite.vel.y -= JUMP_POWER
+
+
+
         if interaction.get_key("a"):
             self.sprite.pos.x -= SPEED
+            self.spritesheet.pos.x -= SPEED
+            self.spritesheet.next_frame()
         if interaction.get_key("d"):
             self.sprite.pos.x += SPEED
+            self.spritesheet.pos.x += SPEED
+            self.spritesheet.next_frame()
+
 
         self.sprite.update()
+
+
 
     def heal(self, amount):
         self.health += amount
@@ -46,13 +63,22 @@ class Player:
         return f"{self.name} (Level {self.level}) - Health: {self.health}/{self.max_health}"
 
     def draw(self, canvas):
+
         # self.sprite.draw(canvas)
-        canvas.draw_polygon([
-            (self.sprite.pos.x, self.sprite.pos.y),
-            (self.sprite.pos.x + self.size[0], self.sprite.pos.y),
-            (self.sprite.pos.x + self.size[0], self.sprite.pos.y + self.size[1]),
-            (self.sprite.pos.x, self.sprite.pos.y + self.size[1])],
-            0,
-            "blue",
-            "blue"
-        )
+        # canvas.draw_polygon([
+        #     (self.sprite.pos.x, self.sprite.pos.y),
+        #     (self.sprite.pos.x + self.size[0], self.sprite.pos.y),
+        #     (self.sprite.pos.x + self.size[0], self.sprite.pos.y + self.size[1]),
+        #     (self.sprite.pos.x, self.sprite.pos.y + self.size[1])],
+        #     0,
+        #     "blue",
+        #     "blue"
+        # )
+
+        self.spritesheet.draw(canvas)
+        # self.spritesheet_idle.draw(canvas)
+        # self.spritesheet_right.draw(canvas)
+
+
+
+
--- src/scenes/welcome.py
+++ src/scenes/welcome.py
@@ -9,12 +9,13 @@ def randCol ():
 
 def draw(manager, canvas, clock, frame, interaction):
     frame.set_canvas_background('rgb(0,5,25)')
-    canvas.draw_text('Welcome to Base 27!', (420, 410), 50, 'White','sans-serif')                                        # POSITION IS THE BOTTOM LEFT OF TEXT
+    canvas.draw_text('W e l c o m e  t o  B a s e  2 7 !', (320, 410), 50, 'White','sans-serif')                                        # POSITION IS THE BOTTOM LEFT OF TEXT
+    canvas.draw_text('Press \'spacebar\' to start game', (520, 750), 20, 'Yellow', 'sans-serif')
     # print(frame.get_canvas_textheight('Welcome to Base 27 !',50))
+    if interaction.get_key("space"):
+        manager.switch_scene("main")
 
 
-    # This is how you would switch to the main scene
-    # manager.switch_scene("main")
 
 
 
