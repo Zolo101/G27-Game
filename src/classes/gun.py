f4d27b4f6c9ae4a4c4890cacdaf6c8e2d248adde
Parsifal-The-Searcher <64698314+Parsifal-The-Searcher@users.noreply.github.com>
2024-03-22 17:02:35 +0000

    ds

--- src/classes/gun.py
+++ src/classes/gun.py
@@ -13,25 +13,13 @@ class gun:
         self.player = player
         self.sprite.sheet.pos.x = self.player.sprite.sheet.pos.x 
         self.sprite.sheet.pos.y = self.player.sprite.sheet.pos.y
-        self.sprite.sheet.frame_index[1] = 1
+   
         
             
-    def update(self,interaction):
+    def update(self):
         self.sprite.sheet.pos.x = self.player.sprite.sheet.pos.x + 50
         self.sprite.sheet.pos.y = self.player.sprite.sheet.pos.y -10
-        if interaction.get_key("a") and not self.sprite.blocked["left"]:
-            self.sprite.vel.x -= SPEED
-            # self.sprite.sheet.pos.x -= SPEED
-            self.sprite.sheet.max_c = 4
-            self.sprite.sheet.frame_index[1] = 0                                                       # CHOOSES THE ROW FROM THE SPRITESHEET
-            self.sprite.sheet.next_frame()
-        if interaction.get_key("d") and not self.sprite.blocked["right"]:
-            self.sprite.vel.x += SPEED
-            self.sprite.sheet.max_c = 4
-            # self.sprite.sheet.pos.x += SPEED
 
-            self.sprite.sheet.frame_index[1] = 1                                                        # CHOOSES THH ROW FROM THE SPRITESHEET
-            self.sprite.sheet.next_frame()
 
          
     def draw(self,canvas):
