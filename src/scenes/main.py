2f9731f0b63f9d4f6bc05eb2600c207d521bc256
Parsifal-The-Searcher <64698314+Parsifal-The-Searcher@users.noreply.github.com>
2024-03-26 13:44:01 +0000

    backshory

--- src/scenes/main.py
+++ src/scenes/main.py
@@ -43,7 +43,7 @@ def draw(manager, canvas, clock, frame, interaction):
 
                 #this line of code crushes the game for some reason
                 #responsible for limiting the number of zombies in each wave
-                #cur_zom_num += 1
+                cur_zom_num += 1
 
 
     #makes zombies substantially harder to kill
