d794ed46d898d0a5ba910c2efb2e07ef7f740966
Zolo101 <umadeguy@gmail.com>
2024-03-27 16:47:37 +0000

    Separate sky color into its own function

--- src/game/Sky.py
+++ src/game/Sky.py
@@ -5,7 +5,7 @@ from src.classes.Sprite import Sprite
 from src.classes.Vector import Vector
 from src.classes.Spritesheet import Spritesheet
 
-SPEED = 0.1
+SPEED = 0.01
 OFFSET = 500
 CENTER = Vector(1280 // 2, 600)
 
@@ -32,8 +32,7 @@ class Sky:
 
         self.phase = math.cos(-clock.time * SPEED)
 
-        x = max(0, (self.sun.pos - CENTER).y) / 1
-        self.color = Color(135, 206, 235) - Color(x, x, x)
+        self.color = self.get_sky_colour()
         canvas.draw_circle(self.sun.pos.get_p(), 1, 100, "Yellow")
 
         canvas.draw_circle(self.moon.pos.get_p(), 1, 100, "White")
@@ -41,3 +40,6 @@ class Sky:
 
         frame.set_canvas_background(self.color.__str__())
 
+    def get_sky_colour(self):
+        x = max(0, (self.sun.pos - CENTER).y) / 1
+        return Color(135, 206, 235) - Color(x, x, x)
