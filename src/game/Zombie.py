82f7297511350efd799160b85d55faa6fd616657
rsn558 <ZMAC558@live.rhul.ac.uk>
2024-03-27 17:54:17 +0000

    new zombie

--- src/game/Zombie.py
+++ src/game/Zombie.py
@@ -73,3 +73,72 @@ class Zombie:
         # )
 
         self.sprite.sheet.draw(canvas)
+
+class Zombie1:
+    def __init__(self, x, y, player, bullets,health = 50, max_health = 50,SPEED = 50,):
+        self.bullets = bullets
+        self.size = (1000, 270)
+        self.sprite = Sprite("zombie5", x, y)
+        self.sprite.sheet = Spritesheet(
+            "./assets/ccc.png",
+            571,
+            437, 8, 6,60,150)
+
+        self.sprite.sheet.pos.x = x
+        self.sprite.sheet.pos.y = y
+        self.sprite.sheet.frame_index[1] = 2
+        self.sprite.sheet.max_c = 8
+        self.player = player
+        self.health = health
+        self.max_health = max_health
+        self.SPEED = SPEED
+
+    def update(self):
+        if self.player.sprite.sheet.pos.x < self.sprite.pos.x:
+            self.sprite.pos.x -= self.SPEED
+            self.sprite.sheet.frame_index[1] = 5
+            self.sprite.sheet.next_frame()
+        if self.player.sprite.sheet.pos.x > self.sprite.pos.x:
+            self.sprite.pos.x += self.SPEED
+            self.sprite.sheet.frame_index[1] = 2
+            self.sprite.sheet.next_frame()
+        if (self.sprite.pos.x > self.player.sprite.sheet.pos.x) and (self.sprite.pos.x < self.player.sprite.sheet.pos.x + 20) and (self.sprite.pos.y > self.player.sprite.sheet.pos.y - 80) and (self.sprite.pos.y < self.player.sprite.sheet.pos.y + 80) :
+            self.player.take_damage(1)
+        if self.sprite.blocked["left"] or self.sprite.blocked["right"]:
+            self.sprite.vel.y -= JUMP_POWER
+            self.sprite.sheet.next_frame()
+
+        # self.sprite.sheet.next_frame()
+        self.sprite.sheet.max_c = 8
+        self.sprite.update()
+
+    def progres_dif(self):
+        """ with each wave zombies become tougher """
+        self.health += 20
+        self.max_health +=20
+        self.SPEED += 2
+
+    def zombie_take_damage(self, amount):
+        """ reduce zombie hp """
+        self.health -= amount
+        if self.health < 0:
+            self.health = 0
+
+    def is_alive(self):
+        return self.health > 0
+
+    def draw(self, canvas):
+        """ This gets run on every frame. """
+
+        # debug
+        # canvas.draw_polygon([
+        #     (self.sprite.pos.x, self.sprite.pos.y),
+        #     (self.sprite.pos.x + self.size[0], self.sprite.pos.y),
+        #     (self.sprite.pos.x + self.size[0], self.sprite.pos.y + self.size[1]),
+        #     (self.sprite.pos.x, self.sprite.pos.y + self.size[1])],
+        #     0,
+        #     "red",
+        #     "red"
+        # )
+
+        self.sprite.sheet.draw(canvas)
