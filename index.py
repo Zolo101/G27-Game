96458b2d5666d602ca195082bb0643d4e3b9f3f5
rsn558 <ZMAC558@live.rhul.ac.uk>
2024-03-28 00:35:26 +0000

    game over screen

--- index.py
+++ index.py
@@ -38,7 +38,7 @@ manager.add_scene(game_over)
 manager.add_scene(backstory)
 manager.add_scene(win)
 
-manager.switch_scene("welcome")
+manager.switch_scene("game_over")
 
 clock = Clock()
 interaction = Interaction(simplegui.KEY_MAP | {"shift": 17})
--- src/scenes/game_over.py
+++ src/scenes/game_over.py
@@ -1,25 +1,32 @@
 from src.classes.SceneManager import Scene
 from src.scenes.main import player
+from src.scenes.main import night_count
 
 
 def draw(manager, canvas, clock, frame, interaction):
     """ This gets run on every frame. """
-    frame.set_canvas_background('rgb(255, 0, 10)')
-    canvas.draw_text('G A M E  O V E R', (462, 408), 50, 'Black',
-                     'sans-serif')  # POSITION IS THE BOTTOM LEFT OF TEXT
-    canvas.draw_text('G A M E  O V E R', (465, 410), 50, 'Yellow',
-                     'sans-serif')  # POSITION IS THE BOTTOM LEFT OF TEXT
+    frame.set_canvas_background('rgb(204, 14, 10)')
+    canvas.draw_text('G A M E  O V E R', (357, 248), 75, 'Black')  # POSITION IS THE BOTTOM LEFT OF TEXT
+    canvas.draw_text('G A M E  O V E R', (360, 250), 75, 'Yellow')  # POSITION IS THE BOTTOM LEFT OF TEXT
     canvas.draw_text('Press \'c\' to close game', (540, 750), 20, 'Yellow', 'sans-serif')
     # print(frame.get_canvas_textheight('Welcome to Base 27 !',50))
 
     #Display Score
-    canvas.draw_text(f"Score: {player.score}", (522, 488), 60, "Black")
-    canvas.draw_text(f"Score: {player.score}", (525, 490), 60, "Yellow")
-    # canvas.draw_text(f"Score: {night_count}", (50, 50), 40, 'White', 'sans-serif')
+    # canvas.draw_text(f"Score: {player.score}", (522, 388), 60, "Black")
+    # canvas.draw_text(f"Score: {player.score}", (525, 390), 60, "Yellow")
+
+    canvas.draw_text(f"YOU SURVIVED {player.night} NIGHTS", (277, 413), 60, "Black")
+    canvas.draw_text(f"YOU SURVIVED {player.night} NIGHTS", (280, 415), 60, "Yellow")
 
     #Display Time Survived
-    canvas.draw_text(f"Time Survived: {player.time_survived}", (422, 568), 60, "Black")
-    canvas.draw_text(f"Time Survived: {player.time_survived}", (425, 570), 60, "Yellow") 
+    canvas.draw_text(f"Time Survived: {player.time_survived}", (422, 493), 60, "Black")
+    canvas.draw_text(f"Time Survived: {player.time_survived}", (425, 495), 60, "Yellow")
+
+    canvas.draw_text(f"Score: {player.score}", (522, 573), 60, "Black")
+    canvas.draw_text(f"Score: {player.score}", (525, 575), 60, "Yellow")
+    #
+    # canvas.draw_text(f"YOU SURVIVED {player.time_survived} NIGHTS", (277, 548), 60, "Black")
+    # canvas.draw_text(f"YOU SURVIVED {player.time_survived} NIGHTS", (280, 550), 60, "Yellow")
 
     if interaction.get_key("c"):
         # manager.switch_scene("welcome")
